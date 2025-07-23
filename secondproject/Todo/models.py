d3d1896e901f48fede33b07d01754042a9e3ac89
sreedhanya-ts <sreedhanyats500@gmail.com>
2025-07-23 13:31:03 +0530

    profile table created

--- secondproject/Todo/models.py
+++ secondproject/Todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,blank=True,null=True)
+    image=models.ImageField(upload_to='profile_pics',blank=True,null=True)
+    firstname=models.CharField(max_length=20,blank=True,null=True)
+    lastname = models.CharField(max_length=20,blank=True,null=True)
+    email=models.CharField(max_length=20,blank=True,null=True)
+    phone= models.IntegerField(default=0,blank=True,null=True)
+    def __str__(self):
+        return self.user.username +" profile"
