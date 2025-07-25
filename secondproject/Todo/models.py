929c08304b845a411cde8425752aec5736e0af61
sreedhanya-ts <sreedhanyats500@gmail.com>
2025-07-25 13:38:41 +0530

    todo display

--- secondproject/Todo/models.py
+++ secondproject/Todo/models.py
@@ -10,3 +10,10 @@ class Profile(models.Model):
     phone= models.IntegerField(default=0,blank=True,null=True)
     def __str__(self):
         return self.user.username +" profile"
+class Todo(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,blank=True,null=True)
+    title=models.CharField(max_length=20,blank=True,null=True)
+    deadline=models.DateField(max_length=20,blank=True,null=True)
+    date=models.DateField(auto_now_add=True)
+    def __str__(self):
+        return self.title
--- secondproject/Todo/urls.py
+++ secondproject/Todo/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('',views.homepage,name='home'),
-    path('task/', views.task_view, name='task'),
+    path('',views.homepage,name='homepage'),
+    # path('task/', views.task, name='task'),
+    path('login/' ,views.userlogin,name="login"),
+    path('signup/',views.signup,name="signup"),
+    path('logout/',views.userlogout,name="logout"),
+    path('edit/', views.todoedit, name="edit"),
+    path('addtodo/',views.addtodo,name="addtodo"),
+
 ]
